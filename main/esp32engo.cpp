8fcb5847f189acee3015779cd8fdbea5ee57b8a2
Anton <toncho11@gmail.com>
2023-06-02 12:24:32 +0200

    Preparing code for testing on both devices.

--- main/esp32engo.cpp
+++ main/esp32engo.cpp
@@ -1,34 +1,97 @@
+/*
+
+https://github.com/ActiveLook/Activelook-API-Documentation/blob/main/ActiveLook_API.md
+https://h2zero.github.io/esp-nimble-cpp/
+
+The extension of this file must be .cpp and you need to use 'extern "C" void app_main(void)' in order to use esp-nimble-cpp.
+*/
+
+#include "esp_log.h"
 #include "NimBLEDevice.h"
- 
-// void setup() in Arduino
+#include "EngoServicesCharacteristics.h"
+
+const char* TAG_BLE = "esp32engo";
+
+void print_info(NimBLEClient *pClient)
+{
+	NimBLERemoteService *pService = pClient->getService(DeviceInformationServiceUUID);
+                
+	if (pService != nullptr) {
+		
+		ESP_LOGE(TAG_BLE, "Found DeviceInformationService");
+		NimBLERemoteCharacteristic *pCharacteristic = pService->getCharacteristic(DeviceInformationService_ManufacturerUUID);
+		
+		if (pCharacteristic != nullptr) {
+			
+			ESP_LOGI(TAG_BLE, "Found DeviceInformationService_Manufacturer");
+			std::string value = pCharacteristic->readValue();
+			ESP_LOGI(TAG_BLE, "Manufacturer Name String: %s", value.c_str()); //TODO: activate
+		}
+		else
+		{
+			ESP_LOGE(TAG_BLE, "No characteristic: DeviceInformationService_Manufacturer");
+		}		
+	}
+	else
+	{
+		ESP_LOGE(TAG_BLE, "No service: DeviceInformationService");
+	}
+}
+
+void send_command(NimBLEClient *pClient)
+{
+	NimBLERemoteService *pService = pClient->getService(ActiveLookCommandsInterfaceUUID);
+	
+	if (pService != nullptr) {
+		
+		ESP_LOGI(TAG_BLE, "Found service ActiveLookCommandsInterface");
+		
+		NimBLERemoteCharacteristic *pCharacteristic = pService->getCharacteristic(ActiveLookCommandsInterface_RXActiveLookUUID);
+		
+		if (pCharacteristic != nullptr) { 
+		
+			ESP_LOGI(TAG_BLE, "Found characteristic ActiveLookCommandsInterface_RXActiveLookUUID");
+		}
+		else
+		{
+			ESP_LOGE(TAG_BLE, "No characteristic ActiveLookCommandsInterface_RXActiveLook");
+		}		
+	}
+	else
+	{
+		ESP_LOGE(TAG_BLE, "No service ActiveLookCommandsInterface");
+	}
+	
+	//put header, footer, length
+	
+	//draw rectangle
+}
+
 extern "C" void app_main(void)  
 {
     NimBLEDevice::init("");
     
     NimBLEScan *pScan = NimBLEDevice::getScan();
-    NimBLEScanResults results = pScan->start(10);
+    NimBLEScanResults results = pScan->start(10); //scan for 10 seconds
     
-    NimBLEUUID serviceUuid("ABCD");
+    NimBLEUUID serviceUuid(DeviceInformationServiceUUID); //TODO: better filter by manufacturer ID ending by 0x08F2
     
     for(int i = 0; i < results.getCount(); i++) {
+		
         NimBLEAdvertisedDevice device = results.getDevice(i);
         
-        if (device.isAdvertisingService(serviceUuid)) {
-            NimBLEClient *pClient = NimBLEDevice::createClient();
+        if (device.isAdvertisingService(serviceUuid)) { //TODO: better filter by manufacturer ID ending by 0x08F2
             
-            if (pClient->connect(&device)) {
-                NimBLERemoteService *pService = pClient->getService(serviceUuid);
-                
-                if (pService != nullptr) {
-                    NimBLERemoteCharacteristic *pCharacteristic = pService->getCharacteristic("1234");
-                    
-                    if (pCharacteristic != nullptr) {
-                        std::string value = pCharacteristic->readValue();
-                        // print or do whatever you need with the value
-                    }
-                }
-            } else {
-            // failed to connect
+			NimBLEClient *pClient = NimBLEDevice::createClient();
+            
+            if (pClient->connect(&device)) 
+			{
+				print_info(pClient);
+				send_command(pClient);     
+            } 
+			else 
+			{
+                ESP_LOGE(TAG_BLE, "Could not connect to device!");
             }
             
             NimBLEDevice::deleteClient(pClient);
---
+++ main/include/EngoServicesCharacteristics.h
@@ -0,0 +1,51 @@
+/*
+
+The following services are implemented in the BLE interface (four are standard services and two are 
+custom for dedicated ActiveLook commands and firmware updates).
+
+*/
+
+#include "inttypes.h"
+
+/* Services */
+
+//1
+const uint16_t GenericAttributeUUID = 0x1801; //Without characteristics
+
+//2 Generic Access
+const uint16_t GenericAccessUUID = 0x1800;
+
+//2.1 Characteristics
+const char* GenericAccess_DeviceNameUUID = "00002A00-0000-1000-8000-00805F9B34FB";
+const char* GenericAccess_AppearanceUUID =	"00002A01-0000-1000-8000-00805F9B34FB";
+const char* GenericAccess_PeripheralUUID = "00002A04-0000-1000-8000-00805F9B34FB";
+
+//3 Device Information Service
+const uint16_t DeviceInformationServiceUUID = 0x180A;
+
+//3.1 Characteristics
+const char* DeviceInformationService_ManufacturerUUID	= "00002A29-0000-1000-8000-00805F9B34DB";
+const char* DeviceInformationService_ModelUUID			= "00002A24-0000-1000-8000-00805F9B34DB";
+const char* DeviceInformationService_SerialUUID		    = "00002A25-0000-1000-8000-00805F9B34DB";
+const char* DeviceInformationService_HardwareUUID		= "00002A27-0000-1000-8000-00805F9B34DB";
+const char* DeviceInformationService_FirmwareUUID		= "00002A26-0000-1000-8000-00805F9B34DB";
+const char* DeviceInformationService_SoftwareUUID		= "00002A28-0000-1000-8000-00805F9B34DB";
+
+//4 Battery Service
+const uint16_t BatteryServiceUUID = 0x180F;
+
+//4.1 Characteristics
+const char* BatteryService_BatteryLevelUUID	= "00002A19-0000-1000-8000-00805F9B34FB"; //battery level (in %)
+
+//5 Custom Service ActiveLook Command Interface
+const char* ActiveLookCommandsInterfaceUUID = "0x0783b03e-8535-b5a0-7140-a304d2495cb7";
+
+//5.1 Characteristics
+const char* ActiveLookCommandsInterface_TXActiveLookUUID = "0783b03e-8535-b5a0-7140-a304d2495cb8";
+const char* ActiveLookCommandsInterface_RXActiveLookUUID = "0783b03e-8535-b5a0-7140-a304d2495cbA";
+const char* ActiveLookCommandsInterface_ControlUUID	     = "0783b03e-8535-b5a0-7140-a304d2495cb9";
+const char* ActiveLookCommandsInterface_GestureEventUUID = "0783b03e-8535-b5a0-7140-a304d2495cbb";
+const char* ActiveLookCommandsInterface_TouchEventUUID   = "0783b03e-8535-b5a0-7140-a304d2495cbc";
+
+//6 Custom Service Firmware Update
+const char* FirmwareUpdateServiceUUID = "0000FEF5-0000-1000-8000-00805F9B34FB";
